eba291a76ad61951d15da083a1e373e1fd220e4e
csalvi <crispitagorico@gmail.com>
2023-09-16 11:59:07 +0100

    Add run_tests.yml

--- tests/test_sigkernel.py
+++ tests/test_sigkernel.py
@@ -20,22 +20,18 @@ def I0(x_squared, num_terms, dtype):
 
 
 class TestSigKernel:
-  # if jax.config.jax_enable_x64:
-  #   tol = 1e-5
-  #   dtype = jnp.double
-  # else:
-  #   tol = 1e-2
-  #   dtype = jnp.single
-
-  tol = 1e-4
+  tol = 1e-5
   dtype = jnp.float64
 
-  static_kernel = lambda x, y: linear_kernel(x, y, scale=1.0)
+  static_kernel = lambda x, y: linear_kernel(x, y)
   pde_solver = FiniteDifferenceSolver(static_kernel=static_kernel, multi_gpu=False)
-  signature_kernel = SigKernel(pde_solver=pde_solver, s0=0.0, t0=0.0, S=1.0, T=1.0, ds=1e-3, dt=1e-3,
-                               add_time=False, interpolation="linear")
+  signature_kernel = SigKernel(pde_solver=pde_solver,
+                               s0=0.0, t0=0.0, S=1.0, T=1.0,
+                               ds=1e-3, dt=1e-3,
+                               add_time=False,
+                               interpolation="linear")
 
-  batch_X, batch_Y, length_X, length_Y, dim = 5, 5, 20, 20, 5
+  batch_X, batch_Y, length_X, length_Y, dim = 5, 5, 10, 10, 5
 
   def test_constant_path(self):
     """Inner product with signature of a constant paths is always equal to 1"""
@@ -47,20 +43,20 @@ class TestSigKernel:
 
   def test_padded_path(self):
     """Padding with lastly observed value doesn't change the inner product."""
-    X = jax.random.normal(getkey(), shape=(self.batch_X, self.length_X, self.dim), dtype=self.dtype)
+    X = 1e-1*jax.random.normal(getkey(), shape=(self.batch_X, self.length_X, self.dim), dtype=self.dtype)
     X_pad = jnp.concatenate([X, jnp.tile(X[:, -1:, :], (1, 5, 1))], axis=1)
-    Y = jax.random.normal(getkey(), shape=(self.batch_Y, self.length_Y, self.dim), dtype=self.dtype)
+    Y = 1e-1*jax.random.normal(getkey(), shape=(self.batch_Y, self.length_Y, self.dim), dtype=self.dtype)
     k_mat = self.signature_kernel.kernel_matrix(X, Y)
     k_mat_pad = self.signature_kernel.kernel_matrix(X_pad, Y)
     assert jnp.allclose(k_mat, k_mat_pad, atol=self.tol)
 
   def test_linear_paths(self):
     """signature kernel of linear paths is equal to the modified Bessel function of order 0"""
-    X = jax.random.normal(getkey(), shape=(self.batch_X, 2, self.dim), dtype=self.dtype)
-    Y = jax.random.normal(getkey(), shape=(self.batch_Y, 2, self.dim), dtype=self.dtype)
+    X = 5e-1*jax.random.normal(getkey(), shape=(self.batch_X, 2, self.dim), dtype=self.dtype).cumsum(axis=1)
+    Y = 5e-1*jax.random.normal(getkey(), shape=(self.batch_Y, 2, self.dim), dtype=self.dtype).cumsum(axis=1)
     k_mat = self.signature_kernel.kernel_matrix(X, Y)
     X_inc = X[:, 1, :] - X[:, 0, :]
     Y_inc = Y[:, 1, :] - Y[:, 0, :]
     ip_inc_mat = jnp.einsum('ik,jk->ij', X_inc, Y_inc)
-    k_mat_bessel = I0(4.0 * ip_inc_mat, num_terms=30, dtype=self.dtype)
+    k_mat_bessel = I0(4.0 * ip_inc_mat, num_terms=50, dtype=self.dtype)
     assert jnp.allclose(k_mat, k_mat_bessel, atol=self.tol)
